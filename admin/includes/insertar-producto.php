05e3bce15f99118b5105e79fe732c9e5481f4a33
Moyo2100 <moises.rojas@leon.uia.mx>
2015-01-29 09:36:58 -0600

    Grupo G
    Proyecto con la posibilidad de agregar una fecha y subir una imagen

--- admin/includes/insertar-producto.php
+++ admin/includes/insertar-producto.php
@@ -5,8 +5,17 @@ $nombre=$_POST['nombre_producto'];
 $descripcion=$_POST['descripcion_producto'];
 $precio=$_POST['precio'];
 $categoria=$_POST['categoria'];
+$fecha=$_POST['fecha_lanzamiento'];
+
+echo $nombre_archivo=$_FILES['imagen_producto']['name'];
+echo $tipo_archivo=$_FILES['imagen_producto']['type'];
+echo $tmp_archivo=$_FILES['imagen_producto']['tmp_name'];
+$directorio_archivos="../img/portadas/";
+
+move_uploaded_file($tmp_archivo, $directorio_archivos . $nombre_archivo);
+
 $consulta_insertar="INSERT INTO Productos
-(clave_producto,nombre_producto,descripcion_producto,precio,categoria)
-VALUES ('$clave','$nombre','$descripcion','$precio','$categoria')";
+(clave_producto,nombre_producto,descripcion_producto,imagen_producto,precio,categoria,fecha_lanzamiento)
+VALUES ('$clave','$nombre','$descripcion','$nombre_archivo','$precio','$categoria','$fecha')";
 mysqli_query($conexion,$consulta_insertar);
 ?>
--- admin/nuevo-producto.php
+++ admin/nuevo-producto.php
@@ -6,17 +6,46 @@ $titulo="Nuevo Producto - Administrador";
 	<head>
 		<meta charset="utf-8">
 		<title><?php echo $titulo; ?></title>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+		  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+		  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+		
+		<script>
+		  $(function() {
+		    $( "#fecha_lanzamiento" ).datepicker(
+				{
+				  showOn: "both",
+				  buttonText: "Calendario",
+				  dateFormat: "yy-mm-dd",
+				  yearRange: "1982:2015",
+				  showOtherMonths: true,
+				  changeMonth: true,
+				  changeYear: true,
+				  dayNames: [ "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado" ],
+				  dayNamesMin: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa" ],
+				  monthNamesShort: [ "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic" ]
+				}
+		    );
+		  });
+		  </script>
+		
 	</head>
 	<body>
 		<h1><?php echo $titulo; ?></h1>
 		
-		<form action="includes/insertar-producto.php" method="POST">
+		<form action="includes/insertar-producto.php" method="POST" enctype="multipart/form-data">
 			
 			<label for="clave_producto">Clave del producto</label>
 			<input type="text" name="clave_producto" value="" id="clave_producto"><br>
 			
 			<label for="nombre_producto">Nombre del producto</label>
 			<input type="text" name="nombre_producto" value="" id="nombre_producto"><br>
+			
+			<label for="fecha_lanzamiento">Fecha de lanzamiento</label>
+			<input type="text" name="fecha_lanzamiento" value="" id="fecha_lanzamiento"><br>
+			
+			<label for="imagen_producto">Portada</label>
+			<input type="file" name="imagen_producto" id="imagen_producto"><br>
 			
 			<label for="descripcion_producto">Descripcion del producto</label>
 			<textarea name="descripcion_producto" id="descripcion_producto"></textarea><br>
